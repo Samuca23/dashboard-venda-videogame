d5eb7abe4fdd70a9a7fbfd197065ec8aa34b9bd3
samuca23 <samuelchiodini@hotmail.com>
2023-07-03 21:28:07 -0300

    sidebar

--- app.py
+++ app.py
@@ -10,7 +10,42 @@ def importa_dado():
     df = df.dropna()
     df = df[df['Year'] <= 2016]
     df['Year'] = df['Year'].astype(int)
+    plataformas = ['Wii', 'PS2', 'PC', 'XOne', 'PS4']
+    filtro = df['Platform'].isin(plataformas)
 
-    return df
+    return df[filtro]
 
 df = importa_dado()
+
+#Configurações iniciais
+st.set_page_config(page_title="📊 Dashboard Games", layout="wide")
+st.markdown("<h1 style='text-align: center;'>📊 Dashboard Games</h1>", unsafe_allow_html=True)
+st.markdown('---')
+
+#Sidebar
+st.sidebar.header("Informe o filtro")
+plataforma = st.sidebar.multiselect(
+    "Selecione a plataforma",
+    options=df["Platform"].unique(),
+    default=df["Platform"].unique()
+)
+
+genero = st.sidebar.multiselect(
+    "Selecione a gênero",
+    options=df["Genre"].unique(),
+    default=df["Genre"].unique()
+)
+
+df_selecao = df.query(
+    "Platform == @plataforma & Genre == @genero"
+)
+
+#Remove estilo Streamlit
+remove_st_estilo = """
+    <style>
+        #MainMenu {visibility: hidden}
+        footer {visibility: hidden}
+        header {visibility: hidden}
+    </style>
+"""
+st.markdown(remove_st_estilo, unsafe_allow_html=True)
