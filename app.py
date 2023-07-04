b955e46f1b9f102ea4292a12c235b8bd2ae89b8d
samuca23 <samuelchiodini@hotmail.com>
2023-07-03 21:39:40 -0300

    graficos

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import collections
-import plotly.exceptions as px
+import plotly.express as px
 
 #Importação/Manipulação dos dados
 def importa_dado():
@@ -39,6 +39,28 @@ genero = st.sidebar.multiselect(
 df_selecao = df.query(
     "Platform == @plataforma & Genre == @genero"
 )
+
+#Frequencia Vendas por Ano
+freq_vendas = df_selecao.groupby('Year').count().sort_values('Name', ascending=False).reset_index()[['Year', 'Name']]
+top_10 = freq_vendas.head(10)
+grafico_frequencia = px.bar(top_10, x='Year', y='Name', title="Frequência de Vendas", labels={'Nome', 'Frequencia'}, color_discrete_sequence=px.colors.sequential.Aggrnyl)
+
+
+# 10 jogos mais frequentes
+top_10_freq_jogo = pd.DataFrame(collections.Counter(
+    df_selecao['Name'].tolist()).most_common(10),
+    columns=['Game', 'Frequency']
+)
+
+grafico_top10 = px.bar(top_10_freq_jogo, x='Game', y='Frequency')
+
+coluna1, coluna2 = st.columns(2)
+
+with coluna1:
+    grafico_frequencia
+
+with coluna2:
+    grafico_top10
 
 #Remove estilo Streamlit
 remove_st_estilo = """
